b7e0955270f5ab09d95ea36fcf5a63258a4226ff
Sikhovskyi Vladyslav <wadeblvde@gmail.com>
2025-02-17 14:39:12 +0200

    Created todolist.Base styles. Can delete/add tasks

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import TodoList from "./components/TodoList";
+import "./globals.css";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-bold">Welcome to the Todo List App</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen w-[1440px] max-w-[100vw] mx-auto bg-[#a6c5ed]">
+      <h1 className="text-[60px] font-syne">Todo List Application</h1>
+      <div className="flex justify-center items center mb-[24px]">
+        <p className="font-syne text-[20px]">Write your first task</p>
+      </div>
+      <TodoList />
     </div>
   );
 }
