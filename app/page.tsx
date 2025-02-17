9b6a66a8a4833b5745d6220db6c26fb1b3f37fe6
Sikhovskyi Vladyslav <wadeblvde@gmail.com>
2025-02-17 16:23:35 +0200

    home text centered

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import "./globals.css";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen w-[1440px] max-w-[100vw] mx-auto bg-[#a6c5ed]">
-      <h1 className="text-[60px] font-syne">Todo List Application</h1>
+      <h1 className="text-[60px] font-syne text-center">Todo List Application</h1>
       <div className="flex justify-center items center mb-[24px]">
-        <p className="font-syne text-[20px]">Write your first task</p>
+        <p className="font-syne text-[20px] text-center ">Write your first task</p>
       </div>
       <TodoList />
     </div>
